e0564fb6ddce7d5eb2d410902c30e519b63105d8
Alter Lagos <alter.strife@gmail.com>
2015-09-26 18:08:57 -0300

    queue size for parallel requests is handled from yml file

--- lib/turbius/requests_queue.rb
+++ lib/turbius/requests_queue.rb
@@ -2,22 +2,29 @@ module Turbius
   module RequestsQueue
     class << self
 
-      attr_accessor :hydra
+      attr_accessor :queue_size
 
-      QUEUE_SIZE = 6
-
-      def hydra
-        @hydra ||= Typhoeus::Hydra.new
+      def queue_size
+        @queue_size ||= ENV['queue_size'].to_i
       end
 
       def enqueue(request)
         hydra.queue request
-        run if hydra.queued_requests.size >= QUEUE_SIZE
+        run if hydra.queued_requests.size >= queue_size
       end
 
       def run
         hydra.run
       end
+
+      private
+
+      attr_accessor :hydra
+
+      def hydra
+        @hydra ||= Typhoeus::Hydra.new
+      end
+
     end
 
   end
