fe4e6e1757d4a94aabfe88c3af828691c7a745bd
Alter Lagos <alter.strife@gmail.com>
2015-10-16 18:16:36 -0300

    New city model

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917022407) do
+ActiveRecord::Schema.define(version: 20151016204714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
-  create_table "itineraries", force: true do |t|
+  create_table "cities", force: :cascade do |t|
+    t.string    "name"
+    t.geography "lonlat",     limit: {:srid=>4326, :type=>"point", :geographic=>true}
+    t.datetime  "created_at",                                                          null: false
+    t.datetime  "updated_at",                                                          null: false
+  end
+
+  create_table "itineraries", force: :cascade do |t|
     t.datetime "arrival_date"
     t.datetime "departure_date"
-    t.string   "arrival_station"
-    t.string   "departure_station"
-    t.string   "seat_type"
+    t.string   "arrival_station",   limit: 255
+    t.string   "departure_station", limit: 255
+    t.string   "seat_type",         limit: 255
     t.integer  "free_seats"
     t.integer  "total_seats"
     t.integer  "fare"
@@ -32,19 +40,19 @@ ActiveRecord::Schema.define(version: 20150917022407) do
 
   add_index "itineraries", ["trip_id"], name: "index_itineraries_on_trip_id", using: :btree
 
-  create_table "trips", force: true do |t|
-    t.string   "origin"
-    t.string   "destination"
-    t.boolean  "available",   default: true
+  create_table "trips", force: :cascade do |t|
+    t.string   "origin",      limit: 255
+    t.string   "destination", limit: 255
+    t.boolean  "available",               default: true
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "versions", force: true do |t|
-    t.string   "item_type",      null: false
-    t.integer  "item_id",        null: false
-    t.string   "event",          null: false
-    t.string   "whodunnit"
+  create_table "versions", force: :cascade do |t|
+    t.string   "item_type",      limit: 255, null: false
+    t.integer  "item_id",                    null: false
+    t.string   "event",          limit: 255, null: false
+    t.string   "whodunnit",      limit: 255
     t.text     "object"
     t.datetime "created_at"
     t.text     "object_changes"
---
+++ spec/factories/cities.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :city do
+    name "MyString"
+lonlat "MyString"
+  end
+
+end
